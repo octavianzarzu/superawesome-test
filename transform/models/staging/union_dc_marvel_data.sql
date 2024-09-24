df68db4d3f2570479391f47a2f78791d5034cf2c
Octavian Z <octavian.zarzu@y42.com>
2024-09-24 16:59:04 +0300

    first few models updated

--- transform/models/staging/union_dc_marvel_data.sql
+++ transform/models/staging/union_dc_marvel_data.sql
@@ -1,3 +1,44 @@
+/* 
+
+** dc-data table **
+
+1. The name represents a concatenation of Character (Universe/Comic name). We can extract only the first part (before '(') using split_part, however there might be cases where the name of the character contains '(' also. Let's look at those: 
+
+    SELECT split_part(name, '(', 1) as character_name 
+    FROM "dc-data"
+    GROUP BY ALL 
+    HAVING count(*) > 1;
+
+    # 17 rows returned, of which: 
+    
+    12 have the same alive status (deceased or alive in both comics they appear in) and 
+    5 have a different status (deceased in one comic, alive in another comic).
+
+    The only noticeable entry is of Krypto
+
+    SELECT split_part(name, '(', 1) as character_name, name, alive, appearances 
+    FROM "dc-data"
+    WHERE name like 'Krypto %';
+
+    # Krypto 	            Krypto (New Earth)	                Living Characters	109
+    # Krypto the Earth Dog 	Krypto the Earth Dog (New Earth)	Living Characters	24
+    # Krypto 	            Krypto (Clone) (New Earth)      	Deceased Characters	1
+
+    Even if it's a clone/duplicate entry, the status is different and the additional appearance will be counted towards the total. 
+
+    SELECT split_part(name, '(', 1) as character_name, 
+       sum(appearances) 
+    FROM "marvel-data"
+    GROUP BY character_name
+
+** marvel-data table ** 
+
+1. The same analysis can be done as in the case of the dc-data table above. 
+
+2. Character names are lowercase, whereas in dc-data and comic_characters_info are capitalized. 
+
+*/
+
 WITH 
 clean_dc_data AS 
 (
